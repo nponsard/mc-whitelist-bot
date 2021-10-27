aaa3912a1b1c32d0d8a7a9e6bb3e7bb44fffef0b
Nils Ponsard <nilsponsard@gmail.com>
2021-10-27 14:26:10 +0200

    discord token from conf

--- internal/commands/start/start.go
+++ internal/commands/start/start.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	cli "github.com/jawher/mow.cli"
+	"github.com/nilsponsard/mc-whitelist-bot/internal/config"
 	"github.com/nilsponsard/mc-whitelist-bot/internal/messages"
 	"github.com/nilsponsard/mc-whitelist-bot/pkg/verbosity"
 )
@@ -22,29 +23,40 @@ func Start(job *cli.Cmd) {
 
 	queueMap = make(map[string]chan int)
 
-	token := job.StringArg("TOKEN", "", "Discord token")
-	rconPassword = job.StringArg("RCON_PASSWORD", "", "RCON password")
-
 	// function to execute
 
 	job.Action = func() {
 
-		discord, err := discordgo.New("Bot " + *token)
+		conf := config.GetConfig()
+
+		// create a client
+
+		discord, err := discordgo.New("Bot " + conf.Discord.Token)
 		if err != nil {
 			verbosity.Error(err)
 			cli.Exit(1)
 		}
 
+		// Print a message when the bot is online
+
 		discord.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
-			verbosity.Info("Bot is up!")
+
+			verbosity.Info("Connected as " + s.State.User.Username)
 		})
+
+		// register handler for messageCreate events
+
 		discord.AddHandler(messages.OnCreate)
+
+		// connect to discord servers
 
 		err = discord.Open()
 		if err != nil {
 			verbosity.Error(err)
 			cli.Exit(1)
 		}
+
+		// wait for an interrupt
 
 		stop := make(chan os.Signal, 1)
 		signal.Notify(stop, os.Interrupt)
